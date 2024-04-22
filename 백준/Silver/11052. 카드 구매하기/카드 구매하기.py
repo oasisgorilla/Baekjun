c871d4b802f31d0f121f1be99c6c65d9e2d0419b
Yong-Je Kang <145945463+oasisgorilla@users.noreply.github.com>
2024-04-22 13:30:11 +0900

    [Silver I] Title: 카드 구매하기, Time: 216 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -14,7 +14,7 @@ dpTable = [0] * (n + 1)
 
 for i in range(1, n + 1): # dpTable 커서
     for j in range(1, i + 1): # 현재 채우고 있는 dpTable커서 까지의 j
-        # 카드 i 개를 사는 최대 가격은 P_j 카드팩을 사는 경우와 j - i 최대가격 + P_i 카드팩 가격이다.
+        # 카드 i 개를 사는 최대 가격은 dpTable[i - j] + cardPrice[j]식으로 j를 1부터 i까지 비교해서 갱신
         dpTable[i] = max(dpTable[i], dpTable[i - j] + cardPrice[j])
 
 print(dpTable[n])
