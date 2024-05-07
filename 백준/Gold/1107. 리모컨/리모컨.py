a5bdd037eeb9c4236410a8db2b53896fae770205
Yong-Je Kang <145945463+oasisgorilla@users.noreply.github.com>
2024-05-07 13:09:49 +0900

    [Gold V] Title: 리모컨, Time: 1940 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.py
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -8,7 +8,9 @@ arr = list(map(int, sys.stdin.readline().split()))
 
 min_click = abs(n - 100) # 최소 버튼 클릭 초기화
 
-for channel in range(1000001):
+# 주어진 n값이 500000(최대 input)이고, 남은 버튼이 6인 경우, 666666에서 내려와야 함,
+# 이때 최악의 경우는 9만 남아서 999999에서 내려와야 하는 경우이므로, range는 0 ~ 999999이다. 
+for channel in range(1000000):
     for digit in str(channel):
         if int(digit) in arr:
             break
