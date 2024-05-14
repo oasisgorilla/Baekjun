81d1dd8f0b9395f3cb3cf65e582365edc8286e2d
Yong-Je Kang <145945463+oasisgorilla@users.noreply.github.com>
2024-05-14 10:34:25 +0900

    [Gold V] Title: 암호 만들기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -5,9 +5,9 @@ l, c = map(int, sys.stdin.readline().split())
 
 charArr = list(map(str, sys.stdin.readline().split()))
 
-charArr.sort()
+charArr.sort() # 받은 문자열을 알파벳 오름차순으로 정렬
 
-resArr = list(combinations(charArr, l)) # charArr에서 l만큼 뽑아야 함
+resArr = list(combinations(charArr, l)) # charArr에서 l만큼 뽑아서 조합 만들기
 
 
 for i in range(len(resArr)):
@@ -15,10 +15,10 @@ for i in range(len(resArr)):
     moumCnt = 0
     # i번째 리스트가 1개 이상의 모음과 2개 이상의 자음을 포함하는지 판별
     for j in resArr[i]:
-        if j in 'aeiou':
-            moumCnt += 1
+        if j in 'aeiou': # 모음 string 안에 reaArr[i] 암호조합 글자가 있는지 확인
+            moumCnt += 1 # 있으면, 모음 개수 증가
         else:
-            zaumCnt += 1
+            zaumCnt += 1 # 없으면 해당 암호조합의 글자는 모음이 아닐 것이므로, 자음 개수 증가
     
-    if moumCnt >= 1 and zaumCnt >= 2:
+    if moumCnt >= 1 and zaumCnt >= 2: # 모음 1개, 자음 2개 이상이면 해당 암호 조합 출력
         print(''.join(resArr[i]))
