d534ebacb22300ceb5bbac6b1f903640cc4c5389
Yong-Je Kang <145945463+oasisgorilla@users.noreply.github.com>
2024-08-22 14:22:29 +0900

    [Bronze I] Title: 이름궁합 테스트, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/17269. 이름궁합 테스트/이름궁합 테스트.py
+++ 백준/Bronze/17269. 이름궁합 테스트/이름궁합 테스트.py
@@ -1,37 +1,19 @@
-import sys
+N, M = map(int, input().split())
+A, B = input().split()
 
-input = sys.stdin.readline
+alp = [3, 2, 1, 2, 4, 3, 1, 3, 1, 1, 3, 1,
+       3, 2, 1, 2, 2, 2, 1, 2, 1, 1, 1, 2, 2, 1]
 
-A, B = map(int, input().split())
-nameA, nameB = input().split()
-
-# A와 B를 합치는 과정
-nameAB = []
-
-min_len = min(A, B)
-
+AB = ''
+min_len = min(N, M)
 for i in range(min_len):
-    nameAB.append(nameA[i])
-    nameAB.append(nameB[i])
+    AB += A[i] + B[i]
+AB += A[min_len:] + B[min_len:]
 
-if A > B:
-    nameAB.extend(nameA[min_len:])
-else:
-    nameAB.extend(nameB[min_len:])
+lst = [alp[ord(i)-ord('A')] for i in AB]
 
-string_list = ''.join(nameAB)
+for i in range(N+M-2):
+    for j in range(N+M-1-i):
+        lst[j] += lst[j+1]
 
-# A와 B를 합친 string_list를 integer_list로 변경하는 과정
-base = 65  # 'A'의 아스키 코드
-strokes = [3, 2, 1, 2, 4, 3, 1, 3, 1, 1, 3, 1, 3, 2, 1, 2, 2, 2, 1, 2, 1, 1, 1, 2, 2, 1]
-integer_list = [strokes[ord(c) - base] for c in string_list]
-
-# 이름 궁합 산출
-while len(integer_list) > 2:
-    integer_list = [(integer_list[i - 1] + integer_list[i]) % 10 for i in range(1, len(integer_list))]
-
-# 1의 자리 0 빼고 % 붙여서 출력
-if integer_list[0] == 0:
-    print(f"{integer_list[1]}%")
-else:
-    print(f"{integer_list[0]}{integer_list[1]}%")
+print("{}%".format((lst[0] % 10) * 10 + (lst[1] % 10)))
