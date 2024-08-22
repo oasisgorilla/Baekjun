f9f71571b21e4df4c1af665153c1d3d7cf3a193a
Yong-Je Kang <145945463+oasisgorilla@users.noreply.github.com>
2024-08-22 16:38:09 +0900

    [Silver V] Title: 누울 자리를 찾아라, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
+++ 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
@@ -3,45 +3,35 @@ input = sys.stdin.readline
 
 N = int(input().rstrip())
 
-room = []
-for i in range(N):
-    room.append(list(map(str, input().rstrip())))
-
-visited_x = [[False for _ in range(N)] for _ in range(N)]
-visited_y = [[False for _ in range(N)] for _ in range(N)]
-
-def dfs_x(x, y, cnt, visited_x):
-    # print(f"dfs_x, x: {x} y: {y} cnt: {cnt}")
-    if visited_x[y][x]:
-        return cnt
-
-    cnt += 1
-    visited_x[y][x] = True
-    if x + 1 < N and room[y][x + 1] != 'X' and not visited_x[y][x + 1]:
-        return dfs_x(x + 1, y, cnt, visited_x)
-    return cnt
-
-def dfs_y(x, y, cnt, visited_y):
-    if visited_y[y][x]:
-        return cnt
-    
-    cnt += 1
-    visited_y[y][x] = True
-    if y + 1 < N and room[y + 1][x] != 'X' and not visited_y[y + 1][x]:
-        return dfs_y(x, y + 1, cnt, visited_y)
-    return cnt
-    
+room = [input().rstrip() for _ in range(N)]
 
 horiz = 0
 verti = 0
 
+# 가로 방향 탐색
 for i in range(N):
+    cnt = 0
     for j in range(N):
-        if room[i][j] != 'X':
-            # print(f"탐색 x: {i}, y: {j}, t/f: {visited_y[i][j]}")
-            if dfs_x(j, i, 0, visited_x) >= 2:
+        if room[i][j] == '.':
+            cnt += 1
+        else:
+            if cnt >= 2:
                 horiz += 1
-            if dfs_y(j, i, 0, visited_y) >= 2:
+            cnt = 0
+    if cnt >= 2:
+        horiz += 1
+
+# 세로 방향 탐색
+for j in range(N):
+    cnt = 0
+    for i in range(N):
+        if room[i][j] == '.':
+            cnt += 1
+        else:
+            if cnt >= 2:
                 verti += 1
+            cnt = 0
+    if cnt >= 2:
+        verti += 1
 
 print(f"{horiz} {verti}")
