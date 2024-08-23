b1a671189539320ed62f0cd252b8311555f5ae25
Yong-Je Kang <145945463+oasisgorilla@users.noreply.github.com>
2024-08-23 21:48:19 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
@@ -1,26 +1,9 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 string = input().rstrip()
 
-stack = []
-queue = deque([])
-
-for i in string:
-    stack.append(i)
-    queue.append(i)
-
-isPal = True
-
-for i in range(len(string)):
-    if stack.pop() != queue.popleft():
-        isPal = False
-        break
-    else:
-        continue
-
-if isPal:
+if string == string[::-1]:
     print(1)
 else:
     print(0)
