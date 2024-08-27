9bacd6fcfd1a34a9d7dc9c9d0d9327eacb1270bc
Yong-Je Kang <145945463+oasisgorilla@users.noreply.github.com>
2024-08-28 00:08:30 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1132 ms, Memory: 46744 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,3 +1,20 @@
+"""1트 계수정렬 실패(음수값 고려 안함)"""
+# import sys
+
+# input = sys.stdin.readline
+
+# N = int(input().rstrip())
+
+# nums = [False] * 1000001
+
+# for _ in range(N):
+#     nums[int(input().rstrip())] = True
+
+# for i in range(1, 1000001):
+#     if nums[i] == True:
+#         print(i)
+
+"""음수값 고려한 계수정렬(46744kb, 1192mb)"""
 import sys
 
 input = sys.stdin.readline
@@ -13,3 +30,21 @@ for _ in range(N):
 for i in range(2000001):
     if nums[i] == True:
         print(i - 1000000)
+
+"""sort사용한 코드(76972kb, 1384mb)"""
+# import sys
+
+# input = sys.stdin.readline
+
+# N = int(input().rstrip())
+
+# nums = []
+
+# for _ in range(N):
+#     nums.append(int(input().rstrip()))
+
+# nums.sort()
+
+# for i in range(N):
+#     print(nums[i])
+
