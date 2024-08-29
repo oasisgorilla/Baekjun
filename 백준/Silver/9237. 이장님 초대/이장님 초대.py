08272c2cc1631ba0f51a0cc6127daeb7dfdd8d67
Yong-Je Kang <145945463+oasisgorilla@users.noreply.github.com>
2024-08-29 09:31:38 +0900

    [Silver V] Title: 이장님 초대, Time: 88 ms, Memory: 42036 KB -BaekjoonHub

---
+++ 백준/Silver/9237. 이장님 초대/이장님 초대.py
@@ -0,0 +1,34 @@
+"""1트 실패"""
+# import sys
+# input = sys.stdin.readline
+
+# N = int(input().rstrip())
+
+# sapling = list(map(int, input().split()))
+
+# sapling.sort(reverse=True)
+
+# answer = 0
+
+# for i in range(N, 0, -1):
+#     answer += -(min(0, i - sapling[i - 1])) 
+
+# print(f"{answer + N - 1}")
+
+"""2트(그리디)"""
+import sys
+
+input = sys.stdin.readline
+
+N = int(input().rstrip())
+
+trees = list(map(int, input().split()))
+
+trees.sort(reverse=True)
+
+answer = 0
+
+for i in range(N):
+    answer = max(answer, trees[i] + i) # 나무 자라는데 필요한 일 수 + 나무 심느라 지난 일 수
+
+print(f"{answer + 2}") # 심는날 + 다 자란 다음날 = 2일
