8fd998256104c48e341b6c56b4dae77220223799
Yong-Je Kang <145945463+oasisgorilla@users.noreply.github.com>
2024-10-22 15:03:08 +0900

    [Silver I] Title: 영역 구하기, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,55 +1,52 @@
+"""2트(dfs)"""
+
 import sys
-
-from collections import deque
-
 input = sys.stdin.readline
 
-m, n, k = map(int, input().split()) # m : 세로, n : 가로, k : 직사각형 개수
+M, N, K = map(int, input().rstrip().split()) # y, x, 직사각형 개수
 
-visited = [[0 for _ in range(n)] for _ in range(m)] # 0 : 미방문, 1 <= : 방문
+square = []
+paper = [[0 for _ in range(N)] for _ in range(M)]
 
-for _ in range(k):
-    x1, y1, x2, y2 = map(int, input().split()) # 1 : 좌하, 2 : 우상
-    
-    """
-    아래 for 문에서 처음에 범위를 y2, y1로 설정해줘서 오류가 났었음.
-    for문으로 직사각형을 그리면 무조건 위에서 아래로 내려가게 그려질거라는 나의 착각
-    위로 그려질 수도 있다. 무조건 작은값이 시작이고, 큰 값이 끝이다.
-    """
+for _ in range(K):
+    x1, y1, x2, y2 = map(int, input().rstrip().split())
     for i in range(y1, y2):
         for j in range(x1, x2):
-            visited[i][j] += 1 # 직사각형이 그려진 곳은 방문처리
+                paper[i][j] = 1
 
+visited = [[False for _ in range(N)] for _ in range(M)]
 
-def bfs(y, x):
-    queue = deque()
-    queue.append((y, x))
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
-    dx = [-1, 0, 1, 0]
-    dy = [0, -1, 0, 1]
-    area = 1 # 면적
+def dfs(x, y):
+    stack = [(x, y)]
+    visited[y][x] = True
 
-    while queue:
-        cy, cx = queue.popleft()
-        
+    size = 1
+
+    while stack:
+        cx, cy = stack.pop()
+
         for i in range(4):
-            ny = cy + dy[i]
             nx = cx + dx[i]
-            if (0 <= ny < m) and (0 <= nx < n) and visited[ny][nx] == 0: # 범위 안에 있으면서, 미방문일 경우
-                visited[ny][nx] = 1
-                queue.append((ny, nx))
-                area += 1
-    return area
+            ny = cy + dy[i]
 
-results = [] # 빈공간의 넓이를 담을 배열
+            if 0 <= nx < N and 0 <= ny < M and paper[ny][nx] == 0 and not visited[ny][nx]:
+                visited[ny][nx] = True
+                stack.append((nx, ny))
+                size += 1
 
-for i in range(m):
-    for j in range(n):
-        if visited[i][j] == 0:
-            visited[i][j] += 1
-            results.append(bfs(i, j))
+    return size
+               
+areas = []
 
-print(len(results))
-print(*sorted(results))
+for i in range(M):
+     for j in range(N):
+          if paper[i][j] == 0 and not visited[i][j]:
+               area = dfs(j, i)
+               areas.append(area)
 
-
+areas.sort()
+print(len(areas))
+print(" ".join(map(str, areas)))
