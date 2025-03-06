454d6c527af558ee899be9b5d17fb775c4f5d0c9
Yong-Je Kang <145945463+oasisgorilla@users.noreply.github.com>
2025-03-06 17:56:56 +0900

    [Silver I] Title: 용돈 관리, Time: 532 ms, Memory: 36264 KB -BaekjoonHub

---
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+K = []
+for _ in range(N):
+    K.append(int(input().strip()))
+
+left = min(K)
+right = sum(K)
+answer = right
+
+while left <= right:
+    mid = (left + right) // 2
+    curr = mid
+    count = 1 # 인출 횟수
+
+    for cash in K:
+        if curr < cash: # 인출 금액보다 써야될 금액이 큰 경우
+            curr = mid # curr 초기화
+            count += 1 # 모자라는 금액을 집어넣고 인출
+        curr -= cash
+    
+    if count > M or mid < max(K):
+        left = mid + 1
+    else:
+        right = mid - 1
+        answer = mid
+
+print(answer)
