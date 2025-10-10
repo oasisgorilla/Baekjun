9114d354dbf6872454ed96c9146b45fdd9ccc6bb
Yong-Je Kang <145945463+oasisgorilla@users.noreply.github.com>
2025-10-10 13:50:31 +0900

    [Silver V] Title: 다리 놓기, Time: 88 ms, Memory: 9504 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.js
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.js
@@ -8,19 +8,19 @@ const MAX = 30;
 const dp = Array.from({ length: MAX + 1 }, () => Array(MAX + 1).fill(0n)); 
 
 for (let m = 0; m <= MAX; m++) { 
-    dp[0][m] = 1n; 
+    dp[m][0] = 1n; 
     dp[m][m] = 1n; 
 } 
 
 for (let m = 1; m <= MAX; m++) { 
     for (let n = 1; n < m; n++) { 
-        dp[n][m] = dp[n][m - 1] + dp[n - 1][m - 1]; 
+        dp[m][n] = dp[m - 1][n] + dp[m - 1][n - 1]; 
     } 
 } 
 
 let out = []; 
 for (let t = 1; t <= T; t++) { 
     const [n, m] = input[t].split(' ').map(Number);  
-    out.push(String(dp[n][m])); 
+    out.push(String(dp[m][n])); 
 } 
 console.log(out.join('\n'));
